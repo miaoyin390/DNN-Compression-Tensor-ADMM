96b4020aa85b448b40ecb045e3096a576788d10f
miao.yin <miao.yin@rutgers.edu>
2021-10-28 21:59:52 -0400

    update

--- ttd.py
+++ ttd.py
@@ -8,24 +8,25 @@ from numpy.linalg import svd
 
 
 def ten2tt(x, tt_shapes, tt_ranks):
-    tt_ranks_ = tt_ranks.copy()
+    # tt_ranks_ = tt_ranks.copy()
     d = len(tt_shapes)
     t = x
     tt_cores = []
     for i in range(d - 1):
-        t = np.reshape(t, [tt_ranks_[i] * tt_shapes[i], -1])
+        t = np.reshape(t, [tt_ranks[i] * tt_shapes[i], -1])
         u, s, v = svd(t, full_matrices=False)
-        if s.shape[0] < tt_ranks_[i + 1]:
-            tt_ranks_[i + 1] = s.shape[0]
+        if s.shape[0] < tt_ranks[i + 1]:
+            tt_ranks[i + 1] = s.shape[0]
 
-        u = u[:, :tt_ranks_[i + 1]]
-        s = s[:tt_ranks_[i + 1]]
-        v = v[:tt_ranks_[i + 1], :]
+        u = u[:, :tt_ranks[i + 1]]
+        s = s[:tt_ranks[i + 1]]
+        v = v[:tt_ranks[i + 1], :]
 
-        tt_cores.append(np.reshape(u, [tt_ranks_[i], tt_shapes[i], tt_ranks_[i + 1]]))
+        tt_cores.append(np.reshape(u, [tt_ranks[i], tt_shapes[i], tt_ranks[i + 1]]))
         t = np.dot(np.diag(s), v)
-    t = np.reshape(t, [tt_ranks_[d - 1], tt_shapes[d - 1], tt_ranks_[d]])
+    t = np.reshape(t, [tt_ranks[d - 1], tt_shapes[d - 1], tt_ranks[d]])
     tt_cores.append(t)
+    # print(tt_ranks)
 
     return tt_cores
 
