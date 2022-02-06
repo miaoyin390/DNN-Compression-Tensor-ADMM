d8ae95c463f9f62e911d7d811faeab4a1bfa73c2
myin390 <76754445+myin390@users.noreply.github.com>
2022-02-05 22:18:37 -0500

    update

--- vit_tt.py
+++ vit_tt.py
@@ -26,12 +26,14 @@ from timm.models.vision_transformer import _cfg, Block, Attention, Mlp
 
 import utils
 from TTLinear import TTLinearM, TTLinearR
+from TKLinear import TKLinearM, TKLinearR
 from typing import Type, Any, Callable, Union, List, Optional
 
 
 class TTAttention(Attention):
     def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.,
-                 linear=Union[TTLinearM, TTLinearR], block_id=None, hp_dict=None, dense_dict=None):
+                 linear=Union[TTLinearM, TTLinearR, TKLinearM, TKLinearR],
+                 block_id=None, hp_dict=None, dense_dict=None):
         super().__init__(dim, num_heads, qkv_bias, attn_drop, proj_drop)
         self.num_heads = num_heads
         head_dim = dim // num_heads
@@ -40,26 +42,20 @@ class TTAttention(Attention):
         qkv_name = 'blocks.' + str(block_id) + '.attn.qkv.'
         w_name = qkv_name + 'weight'
         b_name = qkv_name + 'bias'
-        if w_name in hp_dict.ranks.keys():
-            if dense_dict is None:
-                self.qkv = linear(dim, dim * 3, hp_dict.tt_shapes[w_name], hp_dict.ranks[w_name], bias=qkv_bias)
-            else:
-                self.qkv = linear(dim, dim * 3, hp_dict.tt_shapes[w_name], hp_dict.ranks[w_name],
-                                  bias=qkv_bias, from_dense=True, dense_w=dense_dict[w_name],
-                                  dense_b=dense_dict[b_name] if b_name in dense_dict.keys() else None)
+        if w_name in hp_dict.ranks:
+            self.qkv = linear(dim, dim * 3, bias=qkv_bias, hp_dict=hp_dict, name=w_name,
+                              dense_w=None if dense_dict is None else dense_dict[w_name],
+                              dense_b=None if dense_dict is None else dense_dict[b_name])
         else:
-            self.qkv = nn.Linear(dim, dim * 3)
+            self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop)
         proj_name = 'blocks.' + str(block_id) + '.attn.proj.'
         w_name = proj_name + 'weight'
         b_name = proj_name + 'bias'
-        if w_name in hp_dict.ranks.keys():
-            if dense_dict is None:
-                self.proj = linear(dim, dim, hp_dict.tt_shapes[w_name], hp_dict.ranks[w_name])
-            else:
-                self.proj = linear(dim, dim, hp_dict.tt_shapes[w_name], hp_dict.ranks[w_name],
-                                   from_dense=True, dense_w=dense_dict[w_name],
-                                   dense_b=dense_dict[b_name] if b_name in dense_dict.keys() else None)
+        if w_name in hp_dict.ranks:
+            self.proj = linear(dim, dim, hp_dict=hp_dict, name=w_name,
+                               dense_w=None if dense_dict is None else dense_dict[w_name],
+                               dense_b=None if dense_dict is None else dense_dict[b_name])
         else:
             self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
@@ -67,7 +63,8 @@ class TTAttention(Attention):
 
 class TTMlp(Mlp):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.,
-                 linear=Union[TTLinearM, TTLinearR], block_id=None, hp_dict=None, dense_dict=None):
+                 linear=Union[TTLinearM, TTLinearR, TKLinearM, TKLinearR],
+                 block_id=None, hp_dict=None, dense_dict=None):
         super().__init__(in_features, hidden_features, out_features, act_layer, drop)
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
@@ -76,13 +73,10 @@ class TTMlp(Mlp):
         w_name = fc1_name + 'weight'
         b_name = fc1_name + 'bias'
 
-        if w_name in hp_dict.ranks.keys():
-            if dense_dict is None:
-                self.fc1 = linear(in_features, hidden_features, hp_dict.tt_shapes[w_name], hp_dict.ranks[w_name])
-            else:
-                self.fc1 = linear(in_features, hidden_features, hp_dict.tt_shapes[w_name], hp_dict.ranks[w_name],
-                                  from_dense=True, dense_w=dense_dict[w_name],
-                                  dense_b=dense_dict[b_name] if b_name in dense_dict.keys() else None)
+        if w_name in hp_dict.ranks:
+            self.fc1 = linear(in_features, hidden_features, hp_dict=hp_dict, name=w_name,
+                              dense_w=None if dense_dict is None else dense_dict[w_name],
+                              dense_b=None if dense_dict is None else dense_dict[b_name])
         else:
             self.fc1 = nn.Linear(in_features, hidden_features)
 
@@ -91,13 +85,10 @@ class TTMlp(Mlp):
         fc2_name = 'blocks.' + str(block_id) + '.mlp.fc2.'
         w_name = fc2_name + 'weight'
         b_name = fc2_name + 'bias'
-        if w_name in hp_dict.ranks.keys():
-            if dense_dict is None:
-                self.fc2 = linear(hidden_features, out_features, hp_dict.tt_shapes[w_name], hp_dict.ranks[w_name])
-            else:
-                self.fc2 = linear(hidden_features, out_features, hp_dict.tt_shapes[w_name], hp_dict.ranks[w_name],
-                                  from_dense=True, dense_w=dense_dict[w_name],
-                                  dense_b=dense_dict[b_name] if b_name in dense_dict.keys() else None)
+        if w_name in hp_dict.ranks:
+            self.fc2 = linear(hidden_features, out_features, hp_dict=hp_dict, name=w_name,
+                              dense_w=None if dense_dict is None else dense_dict[w_name],
+                              dense_b=None if dense_dict is None else dense_dict[b_name])
         else:
             self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
@@ -106,7 +97,8 @@ class TTMlp(Mlp):
 class TTBlock(Block):
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm,
-                 linear=Union[TTLinearM, TTLinearR], block_id=None, hp_dict=None, dense_dict=None):
+                 linear=Union[TTLinearM, TTLinearR, TKLinearM, TKLinearR],
+                 block_id=None, hp_dict=None, dense_dict=None):
         super().__init__(dim, num_heads, mlp_ratio, qkv_bias, drop, attn_drop, drop_path, act_layer, norm_layer)
         self.norm1 = norm_layer(dim)
         self.attn = TTAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop,
@@ -140,7 +132,7 @@ class TTVisionTransformer(VisionTransformer):
 
 
 def _tt_vit(patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
-            linear=Union[TTLinearM, TTLinearR], hp_dict=None, dense_dict=None, **kwargs):
+            linear=Union[TTLinearM, TTLinearR, TKLinearM, TKLinearR], hp_dict=None, dense_dict=None, **kwargs):
     model = TTVisionTransformer(
         patch_size=patch_size, embed_dim=embed_dim, depth=depth, num_heads=num_heads, mlp_ratio=mlp_ratio,
         qkv_bias=qkv_bias, linear=linear, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
@@ -274,8 +266,88 @@ def ttr_deit_small_patch16_224(hp_dict, pretrained=False, decompose=False, path=
     return model
 
 
+@register_model
+def tkm_deit_tiny_patch16_224(hp_dict, pretrained=False, decompose=False, path=None, **kwargs):
+    if decompose:
+        if path is None:
+            dense_dict = timm.create_model('deit_tiny_patch16_224', pretrained=True).state_dict()
+        elif path.startswith('http'):
+            dense_dict = torch.hub.load_state_dict_from_url(url=path, map_location="cpu", check_hash=True)
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
+    else:
+        dense_dict = None
+    model = _tt_vit(patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+                    linear=TKLinearM, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained and not decompose:
+        state_dict = torch.load(path, map_location='cpu')
+        model.load_state_dict(state_dict)
+    return model
+
+
+@register_model
+def tkr_deit_tiny_patch16_224(hp_dict, pretrained=False, decompose=False, path=None, **kwargs):
+    if decompose:
+        if path is None:
+            dense_dict = timm.create_model('deit_tiny_patch16_224', pretrained=True).state_dict()
+        elif path.startswith('http'):
+            dense_dict = torch.hub.load_state_dict_from_url(url=path, map_location="cpu", check_hash=True)
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
+    else:
+        dense_dict = None
+    model = _tt_vit(patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+                    linear=TKLinearR, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained and not decompose:
+        state_dict = torch.load(path, map_location='cpu')
+        model.load_state_dict(state_dict)
+    return model
+
+
+@register_model
+def tkm_deit_small_patch16_224(hp_dict, pretrained=False, decompose=False, path=None, **kwargs):
+    if decompose:
+        if path is None:
+            dense_dict = timm.create_model('deit_small_patch16_224', pretrained=True).state_dict()
+        elif path.startswith('http'):
+            dense_dict = torch.hub.load_state_dict_from_url(url=path, map_location="cpu", check_hash=True)
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
+    else:
+        dense_dict = None
+    model = _tt_vit(patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
+                    linear=TKLinearM, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained and not decompose:
+        state_dict = torch.load(path, map_location='cpu')
+        model.load_state_dict(state_dict)
+    return model
+
+
+@register_model
+def tkr_deit_small_patch16_224(hp_dict, pretrained=False, decompose=False, path=None, **kwargs):
+    if decompose:
+        if path is None:
+            dense_dict = timm.create_model('deit_small_patch16_224', pretrained=True).state_dict()
+        elif path.startswith('http'):
+            dense_dict = torch.hub.load_state_dict_from_url(url=path, map_location="cpu", check_hash=True)
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
+    else:
+        dense_dict = None
+    model = _tt_vit(patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
+                    linear=TKLinearR, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained and not decompose:
+        state_dict = torch.load(path, map_location='cpu')
+        model.load_state_dict(state_dict)
+    return model
+
+
 if __name__ == '__main__':
-    baseline = 'deit_small_patch16_224'
+    baseline = 'deit_tiny_patch16_224'
     model_name = 'ttm_' + baseline
     hp_dict = utils.get_hp_dict(model_name, '2')
     model = timm.create_model(model_name, hp_dict=hp_dict, decompose=None)
@@ -293,5 +365,4 @@ if __name__ == '__main__':
             # print(name, p.shape)
             base_params += int(np.prod(p.shape))
     print('Baseline # parameters: {}'.format(base_params))
-    print('Compression ratio: {}'.format(base_params/tk_params))
-
+    print('Compression ratio: {}'.format(base_params / tk_params))
