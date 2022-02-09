Fix bugs for the incorrect computation with 'C' and 'M' lib

TTConv.py
@@ -21,14 +21,29 @@ from ttd import ten2tt
 
 
 class TTConv2dM(Module):
-    def __init__(self, in_channels, out_channels,
-                 kernel_size, stride=1, padding=0, dilation=1, groups=1,
-                 bias=True, padding_mode='zeros', hp_dict=None, name=str,
-                 dense_w=None, dense_b=None):
-        # kernel_size = _pair(kernel_size)
-        # stride = _pair(stride)
-        # padding = _pair(padding)
-        # dilation = _pair(dilation)
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: _size_2_t,
+                 stride: _size_2_t = 1,
+                 padding: _size_2_t = 0,
+                 dilation: _size_2_t = 1,
+                 groups: int = 1,
+                 bias: bool = True,
+                 padding_mode: str = 'zeros',
+                 hp_dict: Optional = None,
+                 name: str = None,
+                 dense_w: Tensor = None,
+                 dense_b: Tensor = None,
+                 ):
+        if groups != 1:
+            raise ValueError("groups must be 1 in this mode")
+        if padding_mode != 'zeros':
+            raise ValueError("padding_mode must be zero in this mode")
+        kernel_size = _pair(kernel_size)
+        stride = _pair(stride)
+        padding = _pair(padding)
+        dilation = _pair(dilation)
         super().__init__()
 
         self.tt_shapes = list(hp_dict.tt_shapes[name])
@@ -48,22 +63,24 @@ class TTConv2dM(Module):
 
         self.in_channels = in_channels
         self.out_channels = out_channels
-
         self.tt_ranks = list(hp_dict.ranks[name])
         self.out_tt_ranks = self.tt_ranks[:self.out_tt_order + 1]
         self.in_tt_ranks = self.tt_ranks[self.out_tt_order + 1:]
+
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.transposed = False
+        self.output_padding = _pair(0)
+        self.groups = groups
+        self.padding_mode = padding_mode
 
         self.in_tt_cores = ParameterList([Parameter(torch.Tensor(
             self.in_tt_ranks[i], self.in_tt_shapes[i], self.in_tt_ranks[i + 1]))
             for i in range(self.in_tt_order)])
 
-        self.kernel_size = kernel_size
-        self.stride = stride
-
-        self.core_conv = torch.nn.Conv2d(in_channels=self.in_tt_ranks[0],
-                                         out_channels=self.out_tt_ranks[-1], kernel_size=kernel_size,
-                                         stride=stride, padding=padding, dilation=dilation,
-                                         groups=groups, bias=False, padding_mode=padding_mode)
+        self.core_kernel = Parameter(Tensor(self.out_tt_ranks[-1], self.in_tt_ranks[0], *self.kernel_size))
 
         self.out_tt_cores = ParameterList([Parameter(torch.Tensor(
             self.out_tt_ranks[i], self.out_tt_shapes[i], self.out_tt_ranks[i + 1]))
@@ -78,17 +95,16 @@ class TTConv2dM(Module):
 
         if dense_w is not None:
             w = dense_w.detach().cpu().numpy()
-            kernel_shape = w.shape
-            w = np.reshape(w, [self.out_channels, self.in_channels, -1])
-            tt_shapes = self.out_tt_shapes + [w.shape[-1]] + self.in_tt_shapes
-            tt_cores = ten2tt(w, tt_shapes, self.tt_ranks)
+            w = np.reshape(w, [self.out_channels, self.in_channels, self.kernel_size[0]*self.kernel_size[1]])
+            w = np.transpose(w, [0, 2, 1])
+            tt_cores = ten2tt(w, self.tt_shapes, self.tt_ranks)
 
             for i in range(len(tt_cores)):
                 if i < self.out_tt_order:
                     self.out_tt_cores[i].data = torch.from_numpy(tt_cores[i])
                 elif i == self.out_tt_order:
-                    self.core_conv.weight.data = torch.from_numpy(tt_cores[i]).permute(0, 2, 1).reshape(
-                        self.out_tt_ranks[-1], self.in_tt_ranks[0], kernel_shape[2], kernel_shape[3])
+                    self.core_kernel.data = torch.from_numpy(tt_cores[i]).permute(0, 2, 1).reshape(
+                        self.out_tt_ranks[-1], self.in_tt_ranks[0], self.kernel_size[0], self.kernel_size[1])
                 else:
                     self.in_tt_cores[i - self.out_tt_order - 1].data = torch.from_numpy(tt_cores[i])
 
@@ -100,7 +116,7 @@ class TTConv2dM(Module):
             init.xavier_uniform_(self.out_tt_cores[i])
         for i in range(self.in_tt_order):
             init.xavier_uniform_(self.in_tt_cores[i])
-        self.core_conv.reset_parameters()
+        init.xavier_uniform_(self.core_kernel)
         # init.kaiming_uniform_(self.cores[i], a=math.sqrt(5))
         # if self.bias is not None:
         #     fan_in, _ = init._calculate_fan_in_and_fan_out(weight)
@@ -120,7 +136,7 @@ class TTConv2dM(Module):
                 out.reshape(-1, self.in_tt_shapes[i] * self.in_tt_ranks[i + 1]).t()).t()
         out = out.reshape(batch_size, height, width, self.in_tt_ranks[0]).permute(0, 3, 1, 2)
 
-        out = self.core_conv(out)
+        out = F.conv2d(out, self.core_kernel, None, self.stride, self.padding, self.dilation, self.groups)
         _, _, height_, width_ = out.shape
 
         out = out.permute(0, 2, 3, 1)
@@ -148,10 +164,9 @@ class TTConv2dM(Module):
             tt_flops += a.shape[0] * a.shape[1] * b.shape[1] / 1000 / 1000
         out = out.reshape(batch_size, height, width, self.in_tt_ranks[0]).permute(0, 3, 1, 2)
 
-        out = self.core_conv(out)
+        out = F.conv2d(out, self.core_kernel, None, self.stride, self.padding, self.dilation, self.groups)
         _, _, height_, width_ = out.shape
-        tt_flops += height_ * width_ * self.kernel_size * self.kernel_size * \
-                    self.core_conv.in_channels * self.core_conv.out_channels / 1000 / 1000
+        tt_flops += height_ * width_ * self.core_kernel.numel() / 1000 / 1000
 
         out = out.permute(0, 2, 3, 1)
         for i in range(self.out_tt_order - 1, -1, -1):
@@ -164,15 +179,15 @@ class TTConv2dM(Module):
         if self.bias is not None:
             out += self.bias
 
-        base_flops = height_ * width_ * self.kernel_size * self.kernel_size * self.in_channels * \
+        base_flops = height_ * width_ * self.kernel_size[0] * self.kernel_size[1] * self.in_channels * \
                      self.out_channels / 1000 / 1000
 
         for core in self.in_tt_cores:
             tt_params += core.numel()
         for core in self.out_tt_cores:
             tt_params += core.numel()
-        tt_params += self.core_conv.weight.numel()
-        base_params = self.kernel_size * self.kernel_size * self.in_channels * self.out_channels
+        tt_params += self.core_kernel.numel()
+        base_params = self.kernel_size[0] * self.kernel_size[1] * self.in_channels * self.out_channels
 
         print('baseline # params: {:.2f}K, tt # params: {:.2f}K'.format(base_params / 1000, tt_params / 1000))
         print('baseline # flops: {:.2f}M, tt # flops: {:.2f}M'.format(base_flops, tt_flops))

resnet_inet_tt.py
@@ -108,7 +108,6 @@ class TTBasicBlock(nn.Module):
 
         if self.downsample is not None:
             identity = self.downsample(x)
-
         out += identity
         out = self.relu(out)
 
@@ -175,7 +174,7 @@ class TTBottleneck(nn.Module):
             hp_dict: Optional = None,
             dense_dict: Optional = None
     ) -> None:
-        super(TTBasicBlock, self).__init__()
+        super(TTBottleneck, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         width = int(planes * (base_width / 64.)) * groups
@@ -328,9 +327,7 @@ class TTResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def _forward_impl(self, x: Tensor) -> Tensor:
-        assert x.shape[0] == 1
-        # See note [TorchScript super()]
+    def forward(self, x: Tensor) -> Tensor:
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -344,11 +341,7 @@ class TTResNet(nn.Module):
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
-
         return x
-
-    def forward(self, x: Tensor) -> Tensor:
-        return self._forward_impl(x)
 
     def forward_flops(self, x):
         x = self.conv1(x)
@@ -408,11 +401,14 @@ def _tt_resnet(
 @register_model
 def ttr_resnet18(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
     if decompose:
-        dense_dict = torch.load(path, map_location='cpu')
+        if pretrained:
+            dense_dict = timm.create_model('resnet18', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
     else:
         dense_dict = None
     model = _tt_resnet(TTBasicBlock, [2, 2, 2, 2], conv=TTConv2dR, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
-    if pretrained:
+    if pretrained and not decompose:
         state_dict = torch.load(path, map_location='cpu')
         model.load_state_dict(state_dict)
     return model
@@ -421,11 +417,46 @@ def ttr_resnet18(hp_dict, decompose=False, pretrained=False, path=None, **kwargs
 @register_model
 def ttm_resnet18(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
     if decompose:
-        dense_dict = torch.load(path, map_location='cpu')
+        if pretrained:
+            dense_dict = timm.create_model('resnet18', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
     else:
         dense_dict = None
     model = _tt_resnet(TTBasicBlock, [2, 2, 2, 2], conv=TTConv2dM, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
-    if pretrained:
+    if pretrained and not decompose:
+        state_dict = torch.load(path, map_location='cpu')
+        model.load_state_dict(state_dict)
+    return model
+
+
+@register_model
+def tkc_resnet18(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
+    if decompose:
+        if pretrained:
+            dense_dict = timm.create_model('resnet18', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
+    else:
+        dense_dict = None
+    model = _tt_resnet(TTBasicBlock, [2, 2, 2, 2], conv=TKConv2dC, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
+    if pretrained and not decompose:
+        state_dict = torch.load(path, map_location='cpu')
+        model.load_state_dict(state_dict)
+    return model
+
+
+@register_model
+def tkm_resnet18(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
+    if decompose:
+        if pretrained:
+            dense_dict = timm.create_model('resnet18', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
+    else:
+        dense_dict = None
+    model = _tt_resnet(TTBasicBlock, [2, 2, 2, 2], conv=TKConv2dM, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
+    if pretrained and not decompose:
         state_dict = torch.load(path, map_location='cpu')
         model.load_state_dict(state_dict)
     return model
@@ -434,11 +465,14 @@ def ttm_resnet18(hp_dict, decompose=False, pretrained=False, path=None, **kwargs
 @register_model
 def ttr_resnet34(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
     if decompose:
-        dense_dict = torch.load(path, map_location='cpu')
+        if pretrained:
+            dense_dict = timm.create_model('resnet34', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
     else:
         dense_dict = None
     model = _tt_resnet(TTBasicBlock, [3, 4, 6, 3], conv=TTConv2dR, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
-    if pretrained:
+    if pretrained and not decompose:
         state_dict = torch.load(path, map_location='cpu')
         model.load_state_dict(state_dict)
     return model
@@ -447,11 +481,14 @@ def ttr_resnet34(hp_dict, decompose=False, pretrained=False, path=None, **kwargs
 @register_model
 def ttr_resnet50(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
     if decompose:
-        dense_dict = torch.load(path, map_location='cpu')
+        if pretrained:
+            dense_dict = timm.create_model('resnet50', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
     else:
         dense_dict = None
     model = _tt_resnet(TTBottleneck, [3, 4, 6, 3], conv=TTConv2dR, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
-    if pretrained:
+    if pretrained and not decompose:
         state_dict = torch.load(path, map_location='cpu')
         model.load_state_dict(state_dict)
     return model
@@ -460,11 +497,14 @@ def ttr_resnet50(hp_dict, decompose=False, pretrained=False, path=None, **kwargs
 @register_model
 def tkr_resnet18(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
     if decompose:
-        dense_dict = torch.load(path, map_location='cpu')
+        if pretrained:
+            dense_dict = timm.create_model('resnet18', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
     else:
         dense_dict = None
     model = _tt_resnet(TTBasicBlock, [2, 2, 2, 2], conv=TKConv2dR, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
-    if pretrained:
+    if pretrained and not decompose:
         state_dict = torch.load(path, map_location='cpu')
         model.load_state_dict(state_dict)
     return model
@@ -473,11 +513,14 @@ def tkr_resnet18(hp_dict, decompose=False, pretrained=False, path=None, **kwargs
 @register_model
 def tkr_resnet34(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
     if decompose:
-        dense_dict = torch.load(path, map_location='cpu')
+        if pretrained:
+            dense_dict = timm.create_model('resnet34', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
     else:
         dense_dict = None
     model = _tt_resnet(TTBasicBlock, [3, 4, 6, 3], conv=TKConv2dR, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
-    if pretrained:
+    if pretrained and not decompose:
         state_dict = torch.load(path, map_location='cpu')
         model.load_state_dict(state_dict)
     return model
@@ -486,11 +529,14 @@ def tkr_resnet34(hp_dict, decompose=False, pretrained=False, path=None, **kwargs
 @register_model
 def tkr_resnet50(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
     if decompose:
-        dense_dict = torch.load(path, map_location='cpu')
+        if pretrained:
+            dense_dict = timm.create_model('resnet50', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
     else:
         dense_dict = None
     model = _tt_resnet(TTBottleneck, [3, 4, 6, 3], conv=TKConv2dR, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
-    if pretrained:
+    if pretrained and not decompose:
         state_dict = torch.load(path, map_location='cpu')
         model.load_state_dict(state_dict)
     return model
@@ -499,11 +545,14 @@ def tkr_resnet50(hp_dict, decompose=False, pretrained=False, path=None, **kwargs
 @register_model
 def tkc_resnet50(hp_dict, decompose=False, pretrained=False, path=None, **kwargs):
     if decompose:
-        dense_dict = torch.load(path, map_location='cpu')
+        if pretrained:
+            dense_dict = timm.create_model('resnet50', pretrained=True).state_dict()
+        else:
+            dense_dict = torch.load(path, map_location='cpu')
     else:
         dense_dict = None
     model = _tt_resnet(TTBottleneck, [3, 4, 6, 3], conv=TKConv2dC, hp_dict=hp_dict, dense_dict=dense_dict, **kwargs)
-    if pretrained:
+    if pretrained and not decompose:
         state_dict = torch.load(path, map_location='cpu')
         model.load_state_dict(state_dict)
     return model
@@ -511,29 +560,30 @@ def tkc_resnet50(hp_dict, decompose=False, pretrained=False, path=None, **kwargs
 
 if __name__ == '__main__':
     baseline = 'resnet18'
-    model_name = 'ttm_' + baseline
-    hp_dict = utils.get_hp_dict(model_name, '2', tt_type='special')
-    model = timm.create_model(model_name, hp_dict=hp_dict, decompose=None)
+    model_name = 'tkc_' + baseline
+    hp_dict = utils.get_hp_dict(model_name, '2', tt_type='general')
+    model = timm.create_model(model_name, hp_dict=hp_dict, decompose=True, pretrained=True)
     compr_params = 0
     for name, p in model.named_parameters():
         # if 'conv' in name or 'fc' in name:
-        # print(name, p.shape)
+        print(name, p.shape)
         if p.requires_grad:
             compr_params += int(np.prod(p.shape))
 
-    x = torch.randn(1, 3, 224, 224)
+    x = torch.randn(256, 3, 224, 224)
     _ = model(x)
-    _, compr_flops, base_flops = model.forward_flops(x)
-    base_params = 0
-    model = timm.create_model(baseline)
-    for name, p in model.named_parameters():
-        # if 'conv' in name or 'fc' in name:
-        # print(name, p.shape)
-        if p.requires_grad:
-            base_params += int(np.prod(p.shape))
-    print('Baseline # parameters: {}'.format(base_params))
-    print('Compressed # parameters: {}'.format(compr_params))
-    print('Compression ratio: {:.3f}'.format(base_params / compr_params))
-    print('Baseline # FLOPs: {:.2f}M'.format(base_flops))
-    print('Compressed # FLOPs: {:.2f}M'.format(compr_flops))
-    print('FLOPs ratio: {:.3f}'.format(base_flops / compr_flops))
+    print(compr_params)
+    # _, compr_flops, base_flops = model.forward_flops(x)
+    # base_params = 0
+    # model = timm.create_model(baseline)
+    # for name, p in model.named_parameters():
+    #     # if 'conv' in name or 'fc' in name:
+    #     # print(name, p.shape)
+    #     if p.requires_grad:
+    #         base_params += int(np.prod(p.shape))
+    # print('Baseline # parameters: {}'.format(base_params))
+    # print('Compressed # parameters: {}'.format(compr_params))
+    # print('Compression ratio: {:.3f}'.format(base_params / compr_params))
+    # print('Baseline # FLOPs: {:.2f}M'.format(base_flops))
+    # print('Compressed # FLOPs: {:.2f}M'.format(compr_flops))
+    # print('FLOPs ratio: {:.3f}'.format(base_flops / compr_flops))