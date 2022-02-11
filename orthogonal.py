Fix bugs for the incorrect computation with 'C' and 'M' lib

orthogonal.py
@@ -7,14 +7,14 @@ import torch
 from torch.autograd import Variable
 
 
-def append_double_l2_loss(model, loss, rho):
+def append_double_l2_loss(model, loss, rho, device):
     for name, param in model.named_parameters():
         if 'first_kernel' in name or 'last_kernel' in name or \
                 'first_factor' in name or 'last_factor' in name:
             if param.shape[0] < param.shape[1]:
-                eye = torch.eye(param.shape[0]).cuda()
+                eye = torch.eye(param.shape[0]).to(device)
                 loss += 0.5 * rho * (torch.norm(torch.mm(torch.squeeze(param), torch.squeeze(param).t()) - eye, p=2)) ** 2
             else:
-                eye = torch.eye(param.shape[1]).cuda()
+                eye = torch.eye(param.shape[1]).to(device)
                 loss += 0.5 * rho * (torch.norm(torch.mm(torch.squeeze(param).t(), torch.squeeze(param)) - eye, p=2)) ** 2
     return loss