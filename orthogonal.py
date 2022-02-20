8b3d34318d5688847fd013c883639e37953a99c5
myin390 <76754445+myin390@users.noreply.github.com>
2022-02-19 22:29:36 -0500

    distributed training

--- orthogonal.py
+++ orthogonal.py
@@ -4,13 +4,13 @@
 # Time: 2021/11/15 17:58
 
 import torch
-from torch.autograd import Variable
 
 
 def append_double_l2_loss(model, loss, rho, device):
     for name, param in model.named_parameters():
         if 'first_kernel' in name or 'last_kernel' in name or \
-                'first_factor' in name or 'last_factor' in name:
+                'first_factor' in name or 'last_factor' in name or \
+                'left_kernel' in name:
             if param.shape[0] < param.shape[1]:
                 eye = torch.eye(param.shape[0]).to(device)
                 loss += 0.5 * rho * (torch.norm(torch.mm(torch.squeeze(param), torch.squeeze(param).t()) - eye, p=2)) ** 2
