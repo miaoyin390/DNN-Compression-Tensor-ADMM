0d63795d589200b09376c6dbd215fe923075ed3d
myin390 <76754445+myin390@users.noreply.github.com>
2022-02-21 17:31:01 -0500

    fix bugs for distributed training

--- admm.py
+++ admm.py
@@ -113,18 +113,16 @@ class ADMM:
     def prune_conv_rank_tk(self, z, name):
         tl.set_backend('numpy')
         ranks = self.hp_dict.ranks[name]
-        with torch.no_grad:
-            core_tensor, (last_factor, first_factor) = partial_tucker(z, modes=[0, 1], rank=ranks, init='svd')
-            updated_z = tl.tucker_to_tensor((core_tensor, (last_factor, first_factor)))
+        core_tensor, (last_factor, first_factor) = partial_tucker(z, modes=[0, 1], rank=ranks, init='svd')
+        updated_z = tl.tucker_to_tensor((core_tensor, (last_factor, first_factor)))
 
         return updated_z
 
     def prune_linear_rank_tk(self, z, name):
         tl.set_backend('numpy')
         ranks = self.hp_dict.ranks[name]
-        with torch.no_grad:
-            core_tensor, (last_factor, first_factor) = partial_tucker(z, modes=[0, 1], rank=ranks, init='svd')
-            updated_z = tl.tucker_to_tensor((core_tensor, (last_factor, first_factor)))
+        core_tensor, (last_factor, first_factor) = partial_tucker(z, modes=[0, 1], rank=ranks, init='svd')
+        updated_z = tl.tucker_to_tensor((core_tensor, (last_factor, first_factor)))
 
         return updated_z
 
