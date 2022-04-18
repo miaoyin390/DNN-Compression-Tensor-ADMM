516bc13d25d4de42738fce7a95c7720360498590
myin390 <76754445+myin390@users.noreply.github.com>
2022-04-18 12:21:25 -0400

    micro

--- engines.py
+++ engines.py
@@ -219,7 +219,9 @@ def train(model, args):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
-        model_without_ddp.load_state_dict(checkpoint['model'])
+        model_path = args.resume.split('_')[0] + '_model.pt'
+        # model_without_ddp.load_state_dict(checkpoint['model'])
+        model_without_ddp.load_state_dict(torch.load(model_path, map_location='cpu'))
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
             lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
